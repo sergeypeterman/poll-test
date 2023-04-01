2dce02e15a56bbd8c37d54d0c57fa3f964659750
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-04-01 14:22:45 +0100

    barebone test

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,49 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import React, { Fragment, useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
-
-//почему сосульки и леденцы не назвали наоборот?
+//
+const config = [
+  {
+    name: "Да",
+    id: "yes",
+  },
+  {
+    name: "Нет",
+    id: "no",
+  },
+  {
+    name: "Смешная третья опция",
+    id: "funny_third",
+  },
+];
 
 export default function Home() {
+  const [hasVoted, setHasVoted] = useState(false);
+  const [error, setError] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [results, setResults] = useState(null);
+
+  const handleClick = async (id) => {
+    setIsSubmitting(true);
+    try {
+      const response = await fetch(`/api/create-vote?id=${id}`);
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const result = await response.json();
+      setResults(result);
+      setIsSubmitting(false);
+      setHasVoted(true);
+    } catch (error) {
+      setIsSubmitting(false);
+      setError({
+        error: true,
+        message: error.message,
+      });
+    }
+  };
+
   return (
     <>
       <Head>
@@ -20,11 +56,64 @@ export default function Home() {
         <div className={styles.page}>
           <div className={styles.poll}>
             <h1>Fuck yeah!</h1>
+            <section>
+              <div>
+                <p>
+                Почему сосульки и леденцы не назвали наоборот? {process.env.VAR_TEST}
+                </p>
+
+                <div>
+                  {!hasVoted ? (
+                    <Fragment>
+                      {config.map((item, index) => {
+                        const { name, id } = item;
+
+                        return (
+                          <button
+                            key={index}
+                            onClick={() => handleClick(id)}
+                            disabled={isSubmitting || error}
+                          >
+                            <span>{name}</span>
+                          </button>
+                        );
+                      })}
+                    </Fragment>
+                  ) : (
+                    <Fragment>
+                      {results.data.map((result, index) => {
+                        const { percent, isMax } = result;
+                        const name = config[index].name;
+                        return (
+                          <div key={index}>
+                            <span
+                              style={{
+                                width: `${percent}%`,
+                              }}
+                            />
+                            <span>{name}</span>
+                            <span>{`${percent}%`}</span>
+                          </div>
+                        );
+                      })}
+                    </Fragment>
+                  )}
+                </div>
+                {hasVoted ? <p>{`${results.total} votes`}</p> : null}
+                {error ? <p>{error.message}</p> : null}
+              </div>
+            </section>
           </div>
-          <div className={styles.footer}><a href="https://www.flaticon.com/free-icons/lollipop" title="lollipop icons">Lollipop icons created by Smashicons - Flaticon</a></div>
+          <div className={styles.footer}>
+            <a
+              href="https://www.flaticon.com/free-icons/lollipop"
+              title="lollipop icons"
+            >
+              Lollipop icons created by Smashicons - Flaticon
+            </a>
+          </div>
         </div>
       </main>
-      
     </>
-  )
+  );
 }
